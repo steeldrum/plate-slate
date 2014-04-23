a2a17e0600ebe73b852f8b5521fd530a61ebfd75
steeldrum <noreply>
2014-04-23 19:01:15 +0000

    Update component /ui/details.reel/details.css

--- ui/details.reel/details.js
+++ ui/details.reel/details.js
@@ -1,17 +1,92 @@
-/**
- * @module ui/details.reel
- * @requires montage/ui/component
- */
+
 var Component = require("montage/ui/component").Component;
 
-/**
- * @class Details
- * @extends Component
- */
-exports.Details = Component.specialize(/** @lends Details# */ {
+exports.Details = Component.specialize({
+
     constructor: {
         value: function Details() {
             this.super();
+        }
+    },
+
+
+    _movie: {
+        value: null
+    },
+
+    movie: {
+        set: function (val) {
+            this._movie = val;
+            this.needsDraw = true;
+        },
+        get: function () {
+            return this._movie;
+        }
+    },
+
+    draw: {
+        value: function () {
+            if (this.movie) {
+                //jshint -W106
+                var audience = this.movie.ratings.audience_rating,
+                    critics = this.movie.ratings.critics_rating;
+                //jshint +W106
+                if (audience === "Fresh") {
+                    this.aImage.style.backgroundPosition = '0px 0px';
+                } else if (audience === "Rotten") {
+                    this.aImage.style.backgroundPosition = '0px -25px';
+                } else if (audience === "Certified Fresh") {
+                    this.aImage.style.backgroundPosition = '0px -50px';
+                } else if (audience === "Upright") {
+                    this.aImage.style.backgroundPosition = '0px -75px';
+                } else if (audience === "Spilled") {
+                    this.aImage.style.backgroundPosition = '0px -125px';
+                } else {
+                    this.aImage.style.backgroundPosition = '0px -100px';
+                }
+
+                if (critics === "Fresh"){
+                    this.cImage.style.backgroundPosition = '0px 0px';
+                } else if (critics === "Rotten"){
+                    this.cImage.style.backgroundPosition = '0px -25px';
+                } else if (critics === "Certified Fresh"){
+                    this.cImage.style.backgroundPosition = '0px -50px';
+                } else if (critics === "Upright"){
+                    this.cImage.style.backgroundPosition = '0px -75px';
+                } else if (critics === "Spilled"){
+                    this.cImage.style.backgroundPosition = '0px -125px';
+                } else {
+                    this.cImage.style.backgroundPosition = '0px -100px';
+                }
+                if (this._isDetailsExpanded) {
+                    this._element.classList.add("expanded");
+                } else {
+                    this._element.classList.remove("expanded");
+                }
+            }
+        }
+    },
+
+    handleRentButtonAction: {
+        value: function () {
+            window.open( this.movie.links.alternate );
+        }
+    },
+
+    handleTrailerButtonAction: {
+        value: function () {
+            this.dispatchEventNamed("openTrailer", true, true, {title: this.movie.title});
+        }
+    },
+
+    _isDetailsExpanded: {
+        value: false
+    },
+
+    handleExpandButtonAction: {
+        value: function () {
+            this._isDetailsExpanded = !this._isDetailsExpanded;
+            this.needsDraw = true;
         }
     }
 });
