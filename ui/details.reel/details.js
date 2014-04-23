cb125fb575b04aca83e30386ab0f365dd386e88d
steeldrum <noreply>
2014-04-23 18:58:50 +0000

    Add component details

---
+++ ui/details.reel/details.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/details.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Details
+ * @extends Component
+ */
+exports.Details = Component.specialize(/** @lends Details# */ {
+    constructor: {
+        value: function Details() {
+            this.super();
+        }
+    }
+});
