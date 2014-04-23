78945b39a39003b1825b91e7aa88acc9ffcff6af
steeldrum <noreply>
2014-04-23 18:54:13 +0000

    Add component shopping

---
+++ ui/shopping.reel/shopping.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/shopping.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Shopping
+ * @extends Component
+ */
+exports.Shopping = Component.specialize(/** @lends Shopping# */ {
+    constructor: {
+        value: function Shopping() {
+            this.super();
+        }
+    }
+});
