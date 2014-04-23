315ef9ec08c828b5734c6134cfba62be48b7b7f4
steeldrum <noreply>
2014-04-23 18:43:40 +0000

    Add component categories

---
+++ ui/categories.reel/categories.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/categories.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Categories
+ * @extends Component
+ */
+exports.Categories = Component.specialize(/** @lends Categories# */ {
+    constructor: {
+        value: function Categories() {
+            this.super();
+        }
+    }
+});
